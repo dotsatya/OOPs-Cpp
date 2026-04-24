514fedbd4e367cb862bb168b2147a47013cfb106
dotsatya <satyasundardey5@gmail.com>
2026-04-24 18:38:44 +0530

    stack object in runtime polymorphism

--- Polymorphism/Run-Dynamic-Overriding-Manual/method_.cpp
+++ Polymorphism/Run-Dynamic-Overriding-Manual/method_.cpp
@@ -24,16 +24,36 @@ public:
 
 int main() {
 
-    // // HEAP // RUNTIME // DYNAMIC POLYMORPHISM 
+// // RUNTIME // DYNAMIC POLYMORPHISM 
+  //1. heap object 
     Animal* ptr;
+    ptr = new Animal();
+    ptr->eat();
+    delete ptr;
+
     ptr = new Dog();
     ptr->eat();
-
+    delete ptr;
+    
     ptr = new cat();
     ptr->eat();
     delete ptr;
     
-    // // STACK // COMPILE // STATIC POLYMORPHISM 
+
+  //2. stack object 
+    Animal a;
+    Dog d;
+    cat c;
+
+    Animal* ptr;
+
+    ptr = &d;
+    ptr->eat();  
+
+    ptr = &c;
+    ptr->eat();   
+
+// // STACK // COMPILE // STATIC POLYMORPHISM 
     cat c1;
     c1.eat();
 
